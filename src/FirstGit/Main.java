0f2fa319492fc08f319648ab038a005fe8f87069
Alex Shishov <shishovalex50@gmail.com>
2018-01-31 21:07:59 +0200

    The first commit in IDEA

--- src/FirstGit/Main.java
+++ src/FirstGit/Main.java
@@ -3,5 +3,6 @@ package FirstGit;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from git");
+        System.out.println("first commit");
     }
 }
